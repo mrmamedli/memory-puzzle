9adbae76fc4e667fa521c4aedc8ab53d392427c7
mariiamamedli <mariiamamedli@ya.ru>
2021-08-30 10:39:39 +0300

    new project

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+WIDTH = 360
+HEIGHT = 480
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("memory puzzle")
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BLACK)
+    pygame.display.flip()
+
+pygame.quit()
